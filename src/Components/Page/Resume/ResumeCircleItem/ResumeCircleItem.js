d1a54646cdd3d2c4096a2b0a36d4f4053be3c272
atakanguney <atakanguney58@hotmail.com>
2020-11-27 18:25:19 +0300

    Fix ResumeCircle Item and add ResumeLinItem

--- src/Components/Page/Resume/ResumeCircleItem/ResumeCircleItem.js
+++ src/Components/Page/Resume/ResumeCircleItem/ResumeCircleItem.js
@@ -1,5 +1,32 @@
-import ResumeItem from "../ResumeItem";
+import { useEffect } from "react";
 
-const ResumeCircleItem = () => null;
+import $ from "jquery";
+import "jquery-circle-progress";
+
+function init_chart_circle() {
+  $(".circle-progress").each(function (i, el) {
+    var $el = $(el);
+    $($el).circleProgress({
+      value: $el.data("value"),
+    });
+  });
+}
+
+const ResumeCircleItem = ({ r, title, v }) => {
+  useEffect(() => {
+    init_chart_circle();
+  }, []);
+  return (
+    <div className="yb-box-circle">
+      <div className="circle-progress" data-value={v}>
+        <strong>
+          {r}
+          <i>%</i>
+        </strong>
+      </div>
+      <h5 className="yb-box-circle-title">{title}</h5>
+    </div>
+  );
+};
 
 export default ResumeCircleItem;
--- src/Components/Page/Resume/ResumeLineItem/ResumeLineItem.js
+++ src/Components/Page/Resume/ResumeLineItem/ResumeLineItem.js
@@ -1,3 +1,15 @@
-const ResumeLineItem = () => null;
+const ResumeLineItem = ({ title, pre }) => {
+  return (
+    <div className="yb-resume-skill-item">
+      <h5>
+        {title}{" "}
+        <small className="uk-float-right uk-text-bold uk-visible@s">
+          {pre}%
+        </small>
+      </h5>
+      <progress className="uk-progress" value={pre} max="100"></progress>
+    </div>
+  );
+};
 
 export default ResumeLineItem;
